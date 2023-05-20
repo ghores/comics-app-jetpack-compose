ae8669d68b59e8c1a28c0780a210385acdc96e4e
ghazimoradi <ghazimoradi.amin@gmail.com>
2023-05-20 13:26:09 +0330

    initial commit

---
+++ app/src/main/java/com/ghores/comicsappcompose/MainActivity.kt
@@ -0,0 +1,27 @@
+package com.ghores.comicsappcompose
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.ui.Modifier
+import com.ghores.comicsappcompose.ui.theme.ComicsAppComposeTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            ComicsAppComposeTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colorScheme.background
+                ) {
+
+                }
+            }
+        }
+    }
+}
